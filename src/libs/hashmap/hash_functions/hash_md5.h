84947c40e74016c5adbdbf115fb1799f12c131f5
therealthingy <therealthingy95@gmail.com>
2021-12-30 16:33:38 +0100

    chore: Adds (wrapper for) alternative to hashcity

---
+++ src/libs/hashmap/hash_functions/hash_md5.h
@@ -0,0 +1,7 @@
+// Note: Requires installed libssl-dev + linker flag `-lssl`
+
+#include <openssl/md5.h>
+
+inline void md5hash (const void *s, const size_t len, void *r) {
+  MD5((unsigned char *)s, (size_t)len, (unsigned char *)r);
+}
