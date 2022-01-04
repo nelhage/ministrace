51b9e3910b1cf6dc1ec25a475b37fa3816c8ea42
therealthingy <therealthingy95@gmail.com>
2022-01-04 21:44:53 +0100

    refactor: Revise execution stack console output

--- src/trace/internal/unwind.c
+++ src/trace/internal/unwind.c
@@ -2,12 +2,11 @@
 #include <libunwind-ptrace.h>
 #include <libiberty/demangle.h>								/* or g++ header `cxxabi.h` using `abi::__cxa_demangle` */
 
-#include <stdlib.h>
 #include <stdio.h>
-
-#include "unwind.h"
+#include <stdlib.h>
 
 #include "../../common/error.h"
+#include "unwind.h"
 
 
 void print_backtrace_of_tracee(pid_t pid) {
@@ -23,54 +22,62 @@ void print_backtrace_of_tracee(pid_t pid) {
     void *context = _UPT_create(pid);
 
     /*
-     * Initialize unwind cursor
+     * unw_init_remote(3): Initialize unwind cursor
      *   - pointed to by `cursor` for unwinding the created
      *   - address space identified by `as`;
      *   - `context` void-pointer tells the address space exactly what entity should be unwound
      */
     unw_cursor_t cursor;
     if (0 > unw_init_remote(&cursor, as, context)) {
-        LOG_ERROR_AND_EXIT("Init failed");
+        LOG_ERROR_AND_EXIT("Unwinding init failed");
     }
 
 
     do {
-        unw_word_t offset = 0, pc = 0;
-        /*
-         * Read the value of register `reg` in stackframe identified by `cursor` and store its value in the word pointed to by `pc`
-         */
-        if (0 > unw_get_reg(&cursor, UNW_REG_IP, &pc)) {
-            LOG_ERROR_AND_EXIT("Reading IP failed");
-        }
+    /* -- $$$   TODO: Print so path  (<path>)  $$$ --  */
+        fprintf(stderr, "> ");
 
-        fprintf(stderr, "0x%lx: ", pc);
 
-        char symbol_buf[4096];
+    /* -- Print function + offset in function --  */
         /*
-         * Get name of function which created stackframe identified by `cursor`
+         * unw_get_proc_name(3): Get name of function which created stackframe identified by `cursor`
          *   - `sym` = pointer to a char buffer which will hold the procedure name and
          *   - that is at least `len` bytes long
          *   - `offset` = pointer to word used to return **byte-offset of IP saved in the stackframe (identified by `cursor`), relative to start of procedure**
          */
+        unw_word_t offset = 0;
+        char symbol_buf[4096];
         if (!unw_get_proc_name(&cursor, symbol_buf, sizeof(symbol_buf), &offset)) {
             char *symbol = symbol_buf;
      	    if (!(symbol = cplus_demangle(symbol_buf, 0))) {
       	        symbol = symbol_buf;
     	    }
 
-            fprintf(stderr, "(%s+0x%lx)\n", symbol, offset);
+            fprintf(stderr, "(%s+0x%lx)", symbol, offset);
             if (symbol_buf != symbol) {													// $$$$$$$$$$$$$$$$$$$ TODO: `free` seems to be necessary (dyn. all. mem.) $$$$$$$$$$$$$$$$$$$
 				// fprintf(stderr, "\n\n ---> %s\n\n", symbol);
 				free(symbol);
 				symbol = NULL;
 			}
         } else {
-            fprintf(stderr, "-- no symbol name found\n");
+            fprintf(stderr, "(-- found no symbol)");
         }
 
+
+    /* -- Print IP -- */
         /*
-         * Advances unwind `cursor` to the next older, less deeply nested stackframe
+         * unw_get_reg(3): Read the value of register `reg` in stackframe identified by `cursor` and store its value in the word pointed to by `pc`
          */
+        unw_word_t pc = 0;
+        if (0 > unw_get_reg(&cursor, UNW_REG_IP, &pc)) {
+            LOG_ERROR_AND_EXIT("Reading IP register failed");
+        }
+        fprintf(stderr, " [0x%lx]\n", pc);
+
+
+    /*
+     * unw_step(3): Advances unwind `cursor` to the next older, less deeply nested stackframe
+     */
     } while (unw_step(&cursor) > 0);
 
     /* Destroy context ?? */
