09ff41b98be054b268154fadeea3c479b86535fe
therealthingy <therealthingy95@gmail.com>
2022-03-31 12:44:14 +0200

    test: Fix alloc of pthread arg

--- test/trace_pthread_fork.c
+++ test/trace_pthread_fork.c
@@ -28,13 +28,15 @@
 #define CLI_FORK_OPTION    "--fork"
 #define CLI_PTHREAD_OPTION "--pthread"
 #define CLI_LOOP_OPTION    "--loop"
+#define CLI_TO_FILE        "--to-file"
 #define CLI_HELP_OPTION    "--help"
 
 void usage(char** argv) {
-    fprintf(stderr, "Usage: %s ["CLI_LOOP_OPTION"] ["CLI_FORK_OPTION" | "CLI_PTHREAD_OPTION"]\n", argv[0]);
+    fprintf(stderr, "Usage: %s ["CLI_TO_FILE"] ["CLI_LOOP_OPTION"] ["CLI_FORK_OPTION" | "CLI_PTHREAD_OPTION"]\n", argv[0]);
 }
 
 typedef struct {
+    bool to_file;
     bool loop;
     bool fork;
     bool pthread;
@@ -42,6 +44,7 @@ typedef struct {
 
 int parse_cli_args(int argc, char** argv, cli_args_t* args) {
     /* - Defaults - */
+    args->to_file = false;
     args->loop = false;
     args->fork = false;
     args->pthread = false;
@@ -50,6 +53,11 @@ int parse_cli_args(int argc, char** argv, cli_args_t* args) {
         if (!strcmp(CLI_HELP_OPTION, argv[i])) {
             usage(argv);
             exit(0);
+        }
+
+        if (!strcmp(CLI_TO_FILE, argv[i])) {
+            args->to_file = true;
+            continue;
         }
 
         if (!strcmp(CLI_LOOP_OPTION, argv[i])) {
@@ -72,8 +80,6 @@ int parse_cli_args(int argc, char** argv, cli_args_t* args) {
 
     return (args->fork && args->pthread) ? (-1) : (0);        /* `fork` & `pthread` are mutually eXclusive */
 }
-
-
 
 
 /* -- Signal handlers -- */
@@ -204,47 +210,59 @@ void register_sig_handlers(void(*sig_handler_func_ptr)(int)) {
 
 /* -- Actual test program -- */
 typedef struct {
+    bool to_file;
     char* pname;
     bool loop;
     void(*sig_handler_func_ptr)(int);
 } routine_arg_t;
 
 void* routine(void* arg) {
-    const char* const pname = ((routine_arg_t*)arg)->pname;
-    const bool loop = ((routine_arg_t*)arg)->loop;
-    void(*sig_handler_func_ptr)(int) = ((routine_arg_t*)arg)->sig_handler_func_ptr;
+/* 0. Setup */
+    register_sig_handlers(((routine_arg_t*)arg)->sig_handler_func_ptr);
 
-    register_sig_handlers(sig_handler_func_ptr);
+    FILE *outfile = ((routine_arg_t*)arg)->to_file ?
+                    DIE_WHEN_ERRNO_VPTR( tmpfile() ) :
+                    stdout;
 
+/* 1. Do stuff */
     do {
-        fprintf(stdout, "tid = %5d, pid = %5d, ppid = %5d, pgid = %5d, sid = %5d [%s]\n",
-                gettid(), getpid(), getppid(), getpgid(0), getsid(0), pname);
+        fprintf(outfile, "tid = %5d, pid = %5d, ppid = %5d, pgid = %5d, sid = %5d [%s]\n",
+                gettid(), getpid(), getppid(), getpgid(0), getsid(0), ((routine_arg_t*)arg)->pname);
         nanosleep((const struct timespec[]){{3, 250000000L}}, NULL);
-    } while(loop);
+    } while(((routine_arg_t*)arg)->loop);
+
+/* 2. Cleanup */
+    if (((routine_arg_t*)arg)->to_file) {
+        fclose(outfile);
+    }
 
     return NULL;
 }
 
+void* routine_pthread_wrapper(void* arg) {
+    void* result = routine(arg);
+    free(arg);
+    return result;
+}
+
 
 int main(int argc, char** argv) {
-
     cli_args_t args;
     if (-1 == parse_cli_args(argc, argv, &args)) {
         usage(argv);
         return(1);
     }
 
-    /* Disable IO buffering for stdout */
-    if (0 != setvbuf(stdout, NULL, _IONBF, 0)) {
-        LOG_WARN("Couldn't change buffering for `stdout`");
-    }
 
 /* - fork - */
     if (args.fork) {
         puts("Forking child ...");
         const pid_t child_pid = DIE_WHEN_ERRNO( fork() );
 
-        routine((routine_arg_t[]){{ .pname = (!child_pid) ? "Child" : "Parent", .loop=args.loop, .sig_handler_func_ptr=(!child_pid) ? &child_signal_handler : &parent_signal_handler }});
+        routine((routine_arg_t[]){{ .pname = (!child_pid) ? "Child" : "Parent",
+                                          .to_file = args.to_file,
+                                          .loop=args.loop,
+                                          .sig_handler_func_ptr=(!child_pid) ? &child_signal_handler : &parent_signal_handler }});
 
         if (child_pid) {
             DIE_WHEN_ERRNO( wait(NULL) );         /* Wait for child process to exit */
@@ -254,20 +272,31 @@ int main(int argc, char** argv) {
     } else if (args.pthread) {
         puts("Creating additional thread ...");
         pthread_t t1;
+{
+        routine_arg_t *pthread_routine_arg = DIE_WHEN_ERRNO_VPTR( malloc(sizeof(*pthread_routine_arg)) );
+        pthread_routine_arg->pname = "Thread-1";
+        pthread_routine_arg->to_file = args.to_file;
+        pthread_routine_arg->loop=args.loop;
+        pthread_routine_arg->sig_handler_func_ptr = &child_signal_handler;
 
-        //pthread_create(&t1, NULL, routine, (routine_arg_t[]){{ .pname = "Thread-1", .loop=args.loop, .sig_handler_func_ptr = &child_signal_handler }});
-        // Checking (as shown above) causes it to print "Thread-0" (instead of "Thread-1")
-        if (0 != pthread_create(&t1, NULL, routine, (routine_arg_t[]){{ .pname = "Thread-1", .loop=args.loop, .sig_handler_func_ptr = &child_signal_handler }}) ) {      // NOTE: Child-thread will overwrite parent's signal handler ??
+        if (0 != pthread_create(&t1, NULL, routine_pthread_wrapper, pthread_routine_arg) ) {      // NOTE: Child-thread will overwrite parent's signal handler ??
             LOG_ERROR_AND_EXIT("Couldn't create additional thread!");
         }
+}
 
-        routine((routine_arg_t[]){{ .pname = "Thread-0", .loop=args.loop, .sig_handler_func_ptr = &parent_signal_handler }});
+        routine((routine_arg_t[]){{ .pname = "Thread-0",
+                                          .to_file = args.to_file,
+                                          .loop=args.loop,
+                                          .sig_handler_func_ptr = &parent_signal_handler }});
         pthread_join(t1, NULL);
 
 /* - default (single threaded) - */
     } else {
         puts("Running single threaded ...");
-        routine((routine_arg_t[]){{ .pname = "Thread-0", .loop=args.loop, .sig_handler_func_ptr = &parent_signal_handler }});
+        routine((routine_arg_t[]){{ .pname = "Thread-0",
+                                          .to_file = args.to_file,
+                                          .loop=args.loop,
+                                          .sig_handler_func_ptr = &parent_signal_handler }});
     }
 
     return 0;
